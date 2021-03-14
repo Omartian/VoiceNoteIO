39c489749354107beea359f3366f31bbbc75ff6e
omar-perez-cano-itlighten <omartian21@gmail.com>
2021-03-14 02:22:18 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h3>
+          VoiceNote.io en construcción
+        </h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
